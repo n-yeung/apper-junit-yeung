a7026df8fcf305a5c13d27dafab4eaf8037dc5eb
n-yeung <nathan-r.yeung@gcash.com>
2023-06-08 13:35:28 +0800

    day 1 java unit

---
+++ src/main/java/com/gcash/AccountRepository.java
@@ -0,0 +1,44 @@
+package com.gcash;
+
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.UUID;
+
+public class AccountRepository {
+    private final List<Account> accounts = new ArrayList<>();
+
+    public String createAccount(String name, Double initialBalance) {
+        String id = UUID.randomUUID().toString();
+        Account account = new Account(id, name, initialBalance);
+
+        accounts.add(account);
+
+        return id;
+    }
+    //public String createAccount(String name, Double initialBalance) {return null;}
+
+    public Account getAccount(String id) {
+
+        for (Account account : accounts) {
+            if (account.id().equals(id)) {
+                return account;
+            }
+        }
+
+        return null;
+    }
+    //public Account getAccount(String id) {return null;}
+
+    public void deleteAccount(String id) {
+        for (Account account : accounts) {
+            if (account.id().equals(id)) {
+                accounts.remove(account);
+                return;
+            }
+        }
+    }
+
+    public Integer getNumberOfAccounts() {
+        return accounts.size();
+    }
+}
